f4432198643aeb5f35cf66bc46fa4ae545121b9a
ankhafizov <ankhafizov1998@yandex.ru>
2021-08-29 00:16:55 +0300

    feature separate_clusters_by_size

--- cluster_worker.py
+++ cluster_worker.py
@@ -25,22 +25,24 @@ def remove_small_clusters(threshold_cluster_size = 7000):
             dm.save_tif(mask2d, fn, save_folder)
 
 
-def remove_big_clusters(threshold_cluster_size = 7000):
+def remove_big_clusters():
     input_mask_folder_name = config_helper.get_RandomWalker_mask_img_folder()
-    save_folder = input_mask_folder_name + "_small"
+    mask_big_folder_name = input_mask_folder_name + "_big"
 
-    for rng in tqdm(Z_RANGES):
-        mask3d_initial, _ = dm.assemble_3d_img_stack(input_mask_folder_name, rng)
-        mask3d_big_folder = config_helper.get_RandomWalker_mask_img_folder() + "_big"
-        mask3d_big, file_names  = dm.assemble_3d_img_stack(mask3d_big_folder, rng)
+    mask3d_initial_data = dm.load_all_data(input_mask_folder_name)
+    mask3d_big_data  = dm.load_all_data(mask_big_folder_name)
 
-        mask3d_big = remove_small_clusters(threshold_cluster_size)
-        mask_dif = mask3d_initial - mask3d_big
+    for m_init_meta, m_big_meta in tqdm(zip(mask3d_initial_data, mask3d_big_data)):
+        m_init, _ = m_init_meta
+        m_big, fn = m_big_meta
+        dm.save_tif(m_init - m_big, fn, input_mask_folder_name + "_small")
 
-        for fn, mask2d in zip(file_names, mask_dif):
-            dm.save_tif(mask2d, fn, save_folder)
+
+def separate_clusters_by_size(threshold_cluster_size):
+    remove_small_clusters(threshold_cluster_size)
+    remove_big_clusters()
 
 
 if __name__ == "__main__":
-    remove_small_clusters()
+    # remove_small_clusters()
     remove_big_clusters()
