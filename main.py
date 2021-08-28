593d835c70d910e16889f69365216909ec6553ce
ankhafizov <ankhafizov1998@yandex.ru>
2021-08-29 00:26:58 +0300

    feature separate_clusters_by_size added to configs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cleaner, mask_worker
 from tqdm import tqdm
 
 import config_helper
-import data_manager as dm
+import cluster_worker
 import random_walker_segmentation as rws
 
 
@@ -57,3 +57,7 @@ if __name__=="__main__":
             rws.segment_neurons(MASKED_IMAGES_FOLDER, z_range,
                                 thrs1 = 0.000266, thrs2 = -1.54e-05,
                                 boundary_mask_folder = boundary_mask_folder)
+    if config_helper.open_config()["separate_small_features"]:
+        threshold_cluster_size = \
+            config_helper.open_config()["threshold_cluster_size"]
+        cluster_worker.separate_clusters_by_size(threshold_cluster_size)
